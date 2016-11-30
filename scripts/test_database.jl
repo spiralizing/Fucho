a76cd1ab5bca60775b778938bee402248e9074ed
Alfredo <spiralizing@gmail.com>
2016-11-29 21:41:40 -0600

    got some things done

--- scripts/test_database.jl
+++ scripts/test_database.jl
@@ -1,4 +1,11 @@
 using SQLite
 using DataFrames
-data = SQLite.DB("/media/alfredo/Killer-Rabbit1.5/Fucho/database.sqlite")
-SQLite.query(data, )
+data = SQLite.DB("/home/alfredo/FuchoDatos/database.sqlite")
+tab = SQLite.tables(data)[:name]
+
+players = SQLite.query(data, "SELECT * FROM Player" )
+p_attributes= SQLite.query(data, "SELECT * FROM Player_Attributes" )
+teams = SQLite.query(data, "SELECT * FROM Team" )
+t_attributes = SQLite.query(data, "SELECT * FROM Team_Attributes")
+
+SQLite.query(data,"SELECT * from Match" )
